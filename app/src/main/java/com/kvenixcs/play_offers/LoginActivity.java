655fc2b78033403c44f926899a4325a2687a07b7
Kevin Takano <kevin.takano@gmail.com>
2016-11-20 17:49:21 -0400

    All files

--- app/src/main/java/com/kvenixcs/play_offers/LoginActivity.java
+++ app/src/main/java/com/kvenixcs/play_offers/LoginActivity.java
@@ -1,17 +1,19 @@
 package com.kvenixcs.play_offers;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+
 import android.content.Intent;
+
+
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
@@ -71,14 +73,29 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         progressDialog.show();
 
+
+
         Task<AuthResult> authResultTask = mAuth.signInWithEmailAndPassword(emailTextView.toString(), passwordTextView.toString())
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.cancel();
                         Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
                         if (!task.isSuccessful()) {
                             Log.w(TAG, "signInWithEmail:failed", task.getException());
+
+                            AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
+
+                            // Setting Dialog Title
+                            alertDialog.setTitle("Mensagem");
+
+                            // Setting Dialog Message
+                            alertDialog.setMessage("Erro!");
+
+                            // Showing Alert Message
+                            alertDialog.show();
                         }
+
                         // ...
                     }
                 });
